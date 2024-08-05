962da6d59832982957e02d2d74fa65bcb8702748
LavanyaGowtham2021 <lavanya.anaimuthu@syncfusion.com>
2024-08-05 19:39:11 +0530

    Wasm

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"0aac603420054d9b6ecf6866d825790b4f875a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1481395589"
+    serviceWorkerVersion: "3316878255"
   }
 });
